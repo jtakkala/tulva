3f3f01f639cb82b1475b0de4c80d74bd18a840e1
Jari Takkala <jtakkala@gmail.com>
2013-10-23 14:07:53 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"code.google.com/p/bencode-go"
+)
+
+type Files struct {
+	Length int
+	Md5sum string
+	Path []string
+}
+
+type Info struct {
+	Name string
+	Length int
+	Files []Files
+//	Pieces string
+	PieceLength int "piece length"
+}
+
+type Metainfo struct {
+	Info Info
+	Announce string
+	AnnounceList [][]string "announce-list"
+}
+
+func main() {
+	file, err := os.Open("archlinux-2013.10.01-dual.iso.torrent")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var m Metainfo
+
+	err = bencode.Unmarshal(file, &m)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(m)
+}
