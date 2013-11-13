36ceb54f3daf7c5d838d24a3592e97dc8c5957c5
Jari Takkala <jtakkala@gmail.com>
2013-11-13 16:57:25 -0500

    Compare SHA-1 checksums

--- io.go
+++ io.go
@@ -22,29 +22,46 @@ type IO struct {
 
 // Reads in files and verifies them, returns a map of pieces we already have
 func (io *IO) Verify() {
-	// Create a buffer of size PieceLength
 	pieceLength := io.metaInfo.Info.PieceLength
+
+	// Create a buffer of size PieceLength
 	buf := make([]byte, pieceLength)
 	if len(io.metaInfo.Info.Files) > 0 {
 		// Multiple File Mode
 		var offset int64
-		var j, n int
+		var piece, n, m int
 		var err error
-		for i, file := range(io.metaInfo.Info.Files) {
-			for j, offset = 0, 0; ; j++ {
-				n, err = io.files[i].ReadAt(buf[n:], offset)
+		h := sha1.New()
+		// Iterate over each file
+		for i, file := range io.metaInfo.Info.Files {
+			for offset = 0; offset < int64(file.Length); {
+				// Reset out hash
+				h.Reset()
+				// Read from file at offset, up to buf size or
+				// less if last read was incomplete due to EOF
+				n, err = io.files[i].ReadAt(buf[m:], offset)
 				if err != nil {
 					if err == sysio.EOF {
-						fmt.Printf("%v, %d, %x\n", file, n, buf[:1024])
-//						fmt.Printf("file %v, offset %d, byte %d\n", file, offset, n)
+						fmt.Printf("%s %d\n", file.Path, n)
+						// Reached EOF
+						m += n
 						break
 					}
 					log.Fatal(err)
 				}
-				fmt.Printf("file %v, offset %d, byte %d\n", file, offset, n)
-				fmt.Printf("sha1: %x\n", io.metaInfo.Info.Pieces[j:j + 20])
+				// We have a full buf, generate a hash and compare with
+				// corresponding pieces part of the torrent file
+				h.Write(buf)
+				fmt.Printf("sha1: %x %x\n", h.Sum(nil), io.metaInfo.Info.Pieces[piece:piece + 20])
+				// Increment offset by number of bytes read
 				offset += int64(n)
-				n = 0
+				m = 0
+				// Increment piece by the length of a SHA-1 hash (20 bytes)
+				piece += 20
+			}
+			if (n == pieceLength) {
+				h.Write(buf)
+				fmt.Printf("sha1: %x %x\n", h.Sum(nil), io.metaInfo.Info.Pieces[piece:piece + 20])
 			}
 		}
 	} else {
@@ -58,6 +75,12 @@ func (io *IO) Stop() error {
 	return io.t.Wait()
 }
 
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
 func (io *IO) Init() {
 	if len(io.metaInfo.Info.Files) > 0 {
 		// Multiple File Mode
@@ -65,23 +88,17 @@ func (io *IO) Init() {
 		// Create the directory if it doesn't exist
 		if _, err := os.Stat(directory); os.IsNotExist(err) {
 			err = os.Mkdir(directory, os.ModeDir | os.ModePerm)
-			if err != nil {
-				log.Fatal(err)
-			}
+			checkError(err)
 		}
 		err := os.Chdir(directory)
-		if err != nil {
-			log.Fatal(err)
-		}
+		checkError(err)
 		for _, file := range io.metaInfo.Info.Files {
 			// Create any sub-directories if required
 			if len(file.Path) > 1 {
 				directory = filepath.Join(file.Path[1:]...)
 				if _, err := os.Stat(directory); os.IsNotExist(err) {
 					err = os.MkdirAll(directory, os.ModeDir | os.ModePerm)
-					if err != nil {
-						log.Fatal(err)
-					}
+					checkError(err)
 				}
 			}
 			// Create the file if it doesn't exist
@@ -90,15 +107,11 @@ func (io *IO) Init() {
 			if _, err := os.Stat(path); os.IsNotExist(err) {
 				// Create the file and return a handle
 				fh, err = os.Create(path)
-				if err != nil {
-					log.Fatal(err)
-				}
+				checkError(err)
 			} else {
 				// Open the file and return a handle
 				fh, err = os.Open(path)
-				if err != nil {
-					log.Fatal(err)
-				}
+				checkError(err)
 			}
 			io.files = append(io.files, fh)
 		}
