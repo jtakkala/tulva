e06ea176f3f92cf5c0f830dfda537fa5f9c6814c
Jari Takkala <jtakkala@gmail.com>
2013-11-18 14:33:35 -0500

    Run gofmt and fix sorting to not use a pointer receiver

--- peer.go
+++ peer.go
@@ -9,11 +9,12 @@ import (
 	"launchpad.net/tomb"
 	"log"
 	"net"
+	"sort"
 )
 
 // PeerTuple represents a single IP+port pair of a peer
 type PeerTuple struct {
-	IP net.IP
+	IP   net.IP
 	Port uint16
 }
 
@@ -24,38 +25,39 @@ type Peer struct {
 type PeerManager struct {
 	peersCh <-chan PeerTuple
 	statsCh chan Stats
-	t tomb.Tomb
+	t       tomb.Tomb
 }
 
 type PeerInfo struct {
-	peerId string
-	isActive bool // The peer is connected and unchoked
+	peerId          string
+	isActive        bool // The peer is connected and unchoked
 	availablePieces []int
-	activeRequests map[int]struct{}
-	qtyPiecesNeeded int // The quantity of pieces that this peer has that we haven't yet downloaded. 
-	requestPieceCh chan<- RequestPiece  // Other end is Peer. Used to tell the peer to request a particular piece.  
-	cancelPieceCh chan<- CancelPiece  // Other end is Peer. Used to tell the peer to cancel a particular piece. 
+	activeRequests  map[int]struct{}
+	qtyPiecesNeeded int                 // The quantity of pieces that this peer has that we haven't yet downloaded.
+	requestPieceCh  chan<- RequestPiece // Other end is Peer. Used to tell the peer to request a particular piece.
+	cancelPieceCh   chan<- CancelPiece  // Other end is Peer. Used to tell the peer to cancel a particular piece.
 }
 
 type SortedPeers []PeerInfo
 
-func (sp *SortedPeers) Less(i, j int) bool {
+func (sp SortedPeers) Less(i, j int) bool {
 	return sp[i].qtyPiecesNeeded <= sp[j].qtyPiecesNeeded
 }
 
-func (sp *SortedPeers) Swap(i, j int) {
+func (sp SortedPeers) Swap(i, j int) {
 	tmp := sp[i]
 	sp[i] = sp[j]
 	sp[j] = tmp
 }
 
-func (sp *SortedPeers) Len() int {
-	len(sp)
+func (sp SortedPeers) Len() int {
+	return len(sp)
 }
 
 func sortedPeersByQtyPiecesNeeded(peers map[string]PeerInfo) []PeerInfo {
 	peerInfoSlice := make(SortedPeers, 0)
-	for peerId, peerInfoSlice := range peers {
+	sorted := make([]PeerInfo, 0)
+	for _, peerInfoSlice := range peers {
 		sorted = append(sorted, peerInfoSlice)
 	}
 	sort.Sort(peerInfoSlice)
@@ -94,18 +96,18 @@ func (pm *PeerManager) Run() {
 
 	for {
 		select {
-		case peer := <- pm.peersCh:
+		case peer := <-pm.peersCh:
 			/*
-			_, ok := peers[peer]
-			if ok {
-				// peer already exists
-				fmt.Println("Peer already in map")
-			} else {
-				peers[peer] = "foo"
-			}
+				_, ok := peers[peer]
+				if ok {
+					// peer already exists
+					fmt.Println("Peer already in map")
+				} else {
+					peers[peer] = "foo"
+				}
 			*/
 			fmt.Println(peer)
-		case <- pm.t.Dying():
+		case <-pm.t.Dying():
 			return
 		}
 	}
