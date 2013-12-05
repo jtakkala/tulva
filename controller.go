144abce6aa057fc46e94687ad816a6c0925062c9
Brian Dignan <briandignan@gmail.com>
2013-12-05 14:14:51 -0500

    in controller changed default of maxSimultaneousDownloadsPerPeer from 1 to 2

--- controller.go
+++ controller.go
@@ -131,7 +131,7 @@ func NewController(finishedPieces []bool, pieceHashes [][]byte, diskIOChans Cont
 	cont.rxChans = &ControllerRxChans{diskIOChans, peerManagerChans, peerChans}
 	cont.peers = make(map[string]*PeerInfo)
 	cont.activeRequestsTotals = make([]int, len(finishedPieces))
-	cont.maxSimultaneousDownloadsPerPeer = 1 // only 1 piece at a time
+	cont.maxSimultaneousDownloadsPerPeer = 2 // only 2 pieces at a time
 	return cont
 }
 
