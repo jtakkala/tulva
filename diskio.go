jtakkala/tulva

commit 0d92c62d87827495d68bfdfecf0238ec99c3a8bd

Don't use fmt package and run gofmt

diskio.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"crypto/sha1"
 	"io"
 	"launchpad.net/tomb"
@@ -50,7 +49,7 @@ func (diskio *DiskIO) Verify() []bool {
 	var pieceIndex, n int
 	var err error
 
-	fmt.Printf("Verifying downloaded files")
+	log.Printf("Verifying downloaded files")
 	if len(diskio.metaInfo.Info.Files) > 0 {
 		// Multiple File Mode
 		var m int
@@ -59,7 +58,7 @@ func (diskio *DiskIO) Verify() []bool {
 			for offset := int64(0); ; offset += int64(n) {
 				// Read from file at offset, up to buf size or
 				// less if last read was incomplete due to EOF
-				fmt.Printf(".")
+				log.Printf(".")
 				n, err = diskio.files[i].ReadAt(buf[m:], offset)
 				if err != nil {
 					if err == io.EOF {
@@ -90,12 +89,12 @@ func (diskio *DiskIO) Verify() []bool {
 		for offset := int64(0); ; offset += int64(n) {
 			// Read from file at offset, up to buf size or
 			// less if last read was incomplete due to EOF
-			fmt.Printf(".")
+			log.Printf(".")
 			n, err = diskio.files[0].ReadAt(buf, offset)
 			if err != nil {
 				if err == io.EOF {
 					// Reached EOF
-					fmt.Printf("\n")
+					log.Printf("\n")
 					break
 				}
 				log.Fatal(err)
@@ -244,7 +243,7 @@ func (diskio *DiskIO) requestBlock(block BlockInfo) BlockResponse {
 	log.Println("DiskIO : requestBlock : Started")
 	defer log.Println("DiskIO : requestBlock : Completed")
 
-	offset := int64(int(block.pieceIndex) * diskio.metaInfo.Info.PieceLength + int(block.begin))
+	offset := int64(int(block.pieceIndex)*diskio.metaInfo.Info.PieceLength + int(block.begin))
 	response := BlockResponse{info: block}
 	if len(diskio.metaInfo.Info.Files) == 0 {
 		// Single File Mode

commit 8912e3f7b4747d075c2347376c29b9d93b1eef15

Verify progress output regressed in commit 0d92c62d87827495d68bfdfecf0238ec99c3a8bd. Of course we have to use the fmt package

diskio.go
@@ -7,6 +7,7 @@ package main
 import (
 	"bytes"
 	"crypto/sha1"
+	"fmt"
 	"io"
 	"launchpad.net/tomb"
 	"log"
@@ -58,7 +59,7 @@ func (diskio *DiskIO) Verify() []bool {
 			for offset := int64(0); ; offset += int64(n) {
 				// Read from file at offset, up to buf size or
 				// less if last read was incomplete due to EOF
-				log.Printf(".")
+				fmt.Printf(".")
 				n, err = diskio.files[i].ReadAt(buf[m:], offset)
 				if err != nil {
 					if err == io.EOF {
@@ -89,12 +90,12 @@ func (diskio *DiskIO) Verify() []bool {
 		for offset := int64(0); ; offset += int64(n) {
 			// Read from file at offset, up to buf size or
 			// less if last read was incomplete due to EOF
-			log.Printf(".")
+			fmt.Printf(".")
 			n, err = diskio.files[0].ReadAt(buf, offset)
 			if err != nil {
 				if err == io.EOF {
 					// Reached EOF
-					log.Printf("\n")
+					fmt.Printf("\n")
 					break
 				}
 				log.Fatal(err)
@@ -113,7 +114,7 @@ func (diskio *DiskIO) Verify() []bool {
 			finishedPieces[(pieceIndex / 20)] = diskio.checkHash(buf[:n], pieceIndex)
 		}
 	}
-	log.Println()
+	fmt.Println()
 
 	return finishedPieces
 }