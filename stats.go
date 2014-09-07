b6ae33f22d7dd9b79cfd30deace9cbae012ed238
Jari Takkala <jtakkala@gmail.com>
2014-09-07 18:56:26 +0100

    Re-factor stats, eliminate sync package, +comments
    Locking unnecessary at this time. Add better comments

--- stats.go
+++ stats.go
@@ -7,19 +7,18 @@ package main
 import (
 	"fmt"
 	"log"
-	"sync"
 	"time"
 )
 
 type Stats struct {
-	Left       int
-	Uploaded   int
-	Downloaded int
-	Errors     int
-	peerCh     chan PeerStats
-	diskIOCh   chan int
-	ticker     <-chan time.Time
-	mu         sync.Mutex
+	peerCh   chan PeerStats   // receive stats counters from peers
+	diskIOCh chan int         // receive bytes written from diskIO
+	ticker   <-chan time.Time // print updates every tick
+
+	Left       int // bytes left to download
+	Uploaded   int // total bytes uploaded
+	Downloaded int // total bytes downloaded
+	Errors     int // total errors
 }
 
 func NewStats(bytesLeft int, diskIOCh chan int) *Stats {
@@ -35,7 +34,7 @@ func (s *Stats) Run() {
 	log.Println("Stats : Run : Started")
 	defer log.Println("Stats : Run : Stopped")
 
-	s.ticker = time.Tick(time.Second * 1)
+	s.ticker = time.Tick(time.Second)
 
 	for {
 		select {
@@ -44,9 +43,7 @@ func (s *Stats) Run() {
 			s.Uploaded += stat.write
 			s.Errors += stat.errors
 		case bytesWritten := <-s.diskIOCh:
-			s.mu.Lock()
 			s.Left -= bytesWritten
-			s.mu.Unlock()
 		case <-s.ticker:
 			fmt.Printf("\033[31mDownloaded: %d, Left: %d, Uploaded: %d, Errors: %d\033[0m\n", s.Downloaded, s.Left, s.Uploaded, s.Errors)
 		}
